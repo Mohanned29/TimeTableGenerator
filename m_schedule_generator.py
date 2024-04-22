824235af8a6336e79f683c936d70cffcce849af8
jinxIsOp <154748750+jinxIsOp@users.noreply.github.com>
2024-04-22 18:03:59 +0100

    changing the body

--- m_schedule_generator.py
+++ m_schedule_generator.py
@@ -7,10 +7,8 @@ class ScheduleGenerator:
         self.schedule = []
         self.last_used_room_index = {'Lecture': -1, 'TD': -1, 'TP': -1}
 
-
     def teacher_is_available(self, teacher_name, day):
         return any(teacher['name'] == teacher_name and day in teacher['availability'] for teacher in self.teachers)
-
 
     def find_suitable_teacher(self, module_name, day):
         for teacher in sorted(self.teachers, key=lambda x: next((mod['priority'] for mod in x['modules'] if mod['name'] in module_name), float('inf'))):
@@ -18,15 +16,14 @@ class ScheduleGenerator:
                 return teacher['name']
         return None
 
-
     def room_is_available(self, room_name, day, start_time, end_time):
         return all(not (start_time < t['end'] and end_time > t['start']) for t in self.assigned_times.get((room_name, day), []))
-
 
     def find_time_slot(self, session_type):
         suitable_rooms = [room for room in self.rooms if room['type'] == session_type]
         room_count = len(suitable_rooms)
         start_index = (self.last_used_room_index[session_type] + 1) % room_count
+
         for i in range(room_count):
             room_index = (start_index + i) % room_count
             room = suitable_rooms[room_index]
@@ -39,17 +36,15 @@ class ScheduleGenerator:
         return None, None, None, None
 
 
-    def assign_room(self, room_name, day, start_time, end_time):
-        if (room_name, day) not in self.assigned_times:
-            self.assigned_times[(room_name, day)] = []
-        self.assigned_times[(room_name, day)].append({'start': start_time, 'end': end_time})
-
-
-    def assign_session(self, module_name, session_type, day, start_time, end_time, room):
-        session_details = [day, f"{start_time} - {end_time}", room['name'], module_name, session_type]
-        self.schedule.append(session_details)
-        self.assign_room(room['name'], day, start_time, end_time)
-
+    def assign_session(self, moduleName, session_type, day, start_time, end_time, room):
+        teacher = self.find_suitable_teacher(moduleName, day)
+        if teacher and self.room_is_available(room['name'], day, start_time, end_time):
+            self.schedule.append([day, f"{start_time} - {end_time}", room['name'], moduleName, session_type, teacher])
+            if (room['name'], day) not in self.assigned_times:
+                self.assigned_times[(room['name'], day)] = []
+            self.assigned_times[(room['name'], day)].append({'start': start_time, 'end': end_time})
+        else:
+            print(f"Failed to assign {moduleName} {session_type} on {day} from {start_time} to {end_time}")
 
     def schedule_lectures(self, module):
         for _ in range(module['lectures']):
@@ -57,20 +52,17 @@ class ScheduleGenerator:
             if day:
                 self.assign_session(module['moduleName'], 'Lecture', day, start_time, end_time, room)
 
-
     def schedule_tds(self, module):
         if module.get('td', False):
             day, start_time, end_time, room = self.find_time_slot('TD')
             if day:
                 self.assign_session(module['moduleName'], 'TD', day, start_time, end_time, room)
 
-
     def schedule_tps(self, module):
         if module.get('tp', False):
             day, start_time, end_time, room = self.find_time_slot('TP')
             if day:
                 self.assign_session(module['moduleName'], 'TP', day, start_time, end_time, room)
-
 
     def generate_schedule(self):
         for module_group in self.section['modules']:
