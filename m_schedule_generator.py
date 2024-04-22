3c3c67c6c5262a8feb3b12a4c71270fd902bdb10
jinxIsOp <154748750+jinxIsOp@users.noreply.github.com>
2024-04-22 17:33:25 +0100

    spaces

--- m_schedule_generator.py
+++ m_schedule_generator.py
@@ -7,8 +7,10 @@ class ScheduleGenerator:
         self.schedule = []
         self.last_used_room_index = {'Lecture': -1, 'TD': -1, 'TP': -1}
 
+
     def teacher_is_available(self, teacher_name, day):
         return any(teacher['name'] == teacher_name and day in teacher['availability'] for teacher in self.teachers)
+
 
     def find_suitable_teacher(self, module_name, day):
         for teacher in sorted(self.teachers, key=lambda x: next((mod['priority'] for mod in x['modules'] if mod['name'] in module_name), float('inf'))):
@@ -16,14 +18,15 @@ class ScheduleGenerator:
                 return teacher['name']
         return None
 
+
     def room_is_available(self, room_name, day, start_time, end_time):
         return all(not (start_time < t['end'] and end_time > t['start']) for t in self.assigned_times.get((room_name, day), []))
+
 
     def find_time_slot(self, session_type):
         suitable_rooms = [room for room in self.rooms if room['type'] == session_type]
         room_count = len(suitable_rooms)
         start_index = (self.last_used_room_index[session_type] + 1) % room_count
-
         for i in range(room_count):
             room_index = (start_index + i) % room_count
             room = suitable_rooms[room_index]
@@ -46,11 +49,13 @@ class ScheduleGenerator:
         else:
             print(f"Failed to assign {moduleName} {session_type} on {day} from {start_time} to {end_time}")
 
+
     def schedule_lectures(self, module):
         for _ in range(module['lectures']):
             day, start_time, end_time, room = self.find_time_slot('Lecture')
             if day:
                 self.assign_session(module['moduleName'], 'Lecture', day, start_time, end_time, room)
+
 
     def schedule_tds(self, module):
         if module.get('td', False):
@@ -58,11 +63,13 @@ class ScheduleGenerator:
             if day:
                 self.assign_session(module['moduleName'], 'TD', day, start_time, end_time, room)
 
+
     def schedule_tps(self, module):
         if module.get('tp', False):
             day, start_time, end_time, room = self.find_time_slot('TP')
             if day:
                 self.assign_session(module['moduleName'], 'TP', day, start_time, end_time, room)
+
 
     def generate_schedule(self):
         for module_group in self.section['modules']:
