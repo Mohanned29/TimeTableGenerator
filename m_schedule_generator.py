5b9deea31215b42f668ce1729aedf95550aa36c2
jinxIsOp <154748750+jinxIsOp@users.noreply.github.com>
2024-05-05 18:13:17 +0100

    fixing the groups probleme

--- m_schedule_generator.py
+++ m_schedule_generator.py
@@ -1,104 +1,90 @@
 import random
+
 class ScheduleGenerator:
-    def __init__(self, section, rooms, teachers, global_room_usage):
+    def __init__(self, section, rooms, teachers):
+
         self.section = section
         self.rooms = rooms
         self.teachers = teachers
-        self.schedule = []
-        self.global_room_usage = global_room_usage
-        self.last_used_room_index = {'Lecture': -1, 'TD': -1, 'TP': -1}
-
-
-    def teacher_is_available(self, teacher_name, day):
-        return any(teacher['name'] == teacher_name and day in teacher['availability'] for teacher in self.teachers)
-
+        self.schedule = []  #hedi will store the final schedule li ytaficha fel response
+        self.assigned_lectures = set()  #track les cours li dernahom deja to avoid double booking
+        self.assigned_group_sessions = {}  #track td and tp li dernahom deja to avoid double booking
 
     def find_suitable_teacher(self, module_name, day):
+        #tfiltri teachers who are available on the given day and can teach the module (ex : garici -> analyse -> monday)
         qualified_teachers = [
             teacher for teacher in self.teachers
-            if any(m['name'] in module_name for m in teacher['modules']) and day in teacher['availability']
+            if any(m['name'] == module_name for m in teacher['modules']) and day in teacher['availability']
         ]
+        #ida mkch teachers available, return a placeholder string : "No teacher available"
         if not qualified_teachers:
-            return None
-        qualified_teachers.sort(key=lambda x: next((mod['priority'] for mod in x['modules'] if mod['name'] in module_name), float('inf')))
-        return qualified_teachers[0]['name'] if qualified_teachers else None
+            return "No teacher available"
+        #sort the qualified teachers by their priority for the module ( apr apr )
+        qualified_teachers.sort(key=lambda x: next((mod['priority'] for mod in x['modules'] if mod['name'] == module_name), float('inf')))
+        return qualified_teachers[0]['name']
 
+    def find_available_room(self, session_type, day):
+        #list te3 rooms that match the session type (lecture , td , tp) and are available on the specified day
+        suitable_rooms = [room for room in self.rooms if room['type'] == session_type and day in room['availability']]
+        #using random , khyr room:
+        return random.choice(suitable_rooms)['name'] if suitable_rooms else None
 
-    def room_is_available(self, room_name, day, start_time, end_time):
-        bookings = self.global_room_usage.get((room_name, day), [])
-        for booking in bookings:
-            if start_time < booking['end'] and end_time > booking['start']:
-                return False
-        return True
+    def assign_session(self, group, module, session_type, day):
 
+        #check for previous booking to avoid double booking
+        if session_type == 'Lecture' and module['moduleName'] in self.assigned_lectures:
+            return
+        if group and (module['moduleName'], session_type, group) in self.assigned_group_sessions:
+            return
 
-    def find_time_slot(self, session_type):
-        suitable_rooms = [room for room in self.rooms if room['type'] == session_type]
-        #shuffle rooms to start potentially from a different index each time to distribute room usage evenly
-        random.shuffle(suitable_rooms)
-        for room in suitable_rooms:
-            for availability in room['availability']:
-                day = availability['day']
-                for time in availability['time']:
-                    start_time = time['start']
-                    end_time = time['end']
-                    if self.room_is_available(room['name'], day, start_time, end_time):
-                        # Mark this room as used in this time slot globally
-                        if (room['name'], day, start_time, end_time) not in self.global_room_usage:
-                            self.global_room_usage.setdefault((room['name'], day), []).append({'start': start_time, 'end': end_time})
-                        return day, start_time, end_time, room
-        return None, None, None, None
+        #find an available room and a teacher , assinging la seance
+        room_name = self.find_available_room(session_type, day)
+        if not room_name:
+            return  #exit if no room is available w hedi mtsrach ida kanet data kbira
 
+        teacher = self.find_suitable_teacher(module['moduleName'], day)
+        if teacher == "No teacher available":
+            return  #exit if no teacher is available
 
-    def assign_session(self, moduleName, session_type, day, start_time, end_time, room, group_number=None):
-        if session_type in ['TD', 'TP'] and group_number is not None:
-            teacher = self.find_suitable_teacher(moduleName, day)
-            if not teacher:
-                teacher = "No teacher available"
-            self.schedule.append([day, f"{start_time} - {end_time}", room['name'], moduleName, session_type, f"GROUPE {group_number}", teacher])
-            self.global_room_usage.setdefault((room['name'], day), []).append({'start': start_time, 'end': end_time})
-        else:
-            if self.room_is_available(room['name'], day, start_time, end_time):
-                teacher = self.find_suitable_teacher(moduleName, day)
-                if not teacher:
-                    teacher = "No teacher available"
-                self.schedule.append([day, f"{start_time} - {end_time}", room['name'], moduleName, session_type, teacher])
-                self.global_room_usage.setdefault((room['name'], day), []).append({'start': start_time, 'end': end_time})
-            else:
-                print(f"Failed to assign {moduleName} {session_type} on {day} from {start_time} to {end_time}")
+        #zid la seance details to the schedule list bch tafichiha f response
+        self.schedule.append({
+            'day': day,
+            'room': room_name,
+            'moduleName': module['moduleName'],
+            'session_type': session_type,
+            'teacher': teacher,
+            'group': group
+        })
 
-
-    def schedule_lectures(self, module):
-        for _ in range(module['lectures']):
-            day, start_time, end_time, room = self.find_time_slot('Lecture')
-            if day:
-                self.assign_session(module['moduleName'], 'Lecture', day, start_time, end_time, room)
-
-
-    def schedule_tds(self, module):
-        if module.get('td', False):
-            for group_number, _ in enumerate(self.section['groups'], 1):
-                day, start_time, end_time, room = self.find_time_slot('TD')
-                if day:
-                    self.assign_session(module['moduleName'], 'TD', day, start_time, end_time, room, group_number)
-
-
-    def schedule_tps(self, module):
-        if module.get('tp', False):
-            for group_number, _ in enumerate(self.section['groups'], 1):
-                day, start_time, end_time, room = self.find_time_slot('TP')
-                if day:
-                    self.assign_session(module['moduleName'], 'TP', day, start_time, end_time, room, group_number)
-
+        #mark la seance as assigned to prevent future double booking
+        if session_type == 'Lecture':
+            self.assigned_lectures.add(module['moduleName'])
+        if group:
+            self.assigned_group_sessions[(module['moduleName'], session_type, group)] = True
 
     def generate_schedule(self):
+        #define the days of the week where classes can be scheduled
+        days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Saturday", "Sunday"]
         for module_group in self.section['modules']:
             for module in module_group['modules']:
-                self.schedule_lectures(module)
-                if module.get('td', False) and module.get('tp', False):
-                    self.schedule_tds(module)
-                    self.schedule_tps(module)
-                else:
-                    self.schedule_tds(module)
-                    self.schedule_tps(module)
-        return self.schedule
+                #schedule one lecture for each module and pas plus if not already scheduled
+                for day in days_of_week:
+                    if module['moduleName'] not in self.assigned_lectures:
+                        self.assign_session(None, module, 'Lecture', day)
+                        break  #if we schedule one lecture than nhbso (khdmt beli chaque module 3ndo cour wahed)
+
+                #same for td and tp
+                for group in self.section['groups']:
+                    if module.get('td', False):
+                        for day in days_of_week:
+                            if (module['moduleName'], 'TD', group) not in self.assigned_group_sessions:
+                                self.assign_session(group, module, 'TD', day)
+                                break  #hbs after scheduling one session par groupe
+
+                    if module.get('tp', False):
+                        for day in days_of_week:
+                            if (module['moduleName'], 'TP', group) not in self.assigned_group_sessions:
+                                self.assign_session(group, module, 'TP', day)
+                                break
+
+        return self.schedule  #return the complete schedule
