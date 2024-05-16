7778f7b4432770ad511ecc8ab0d5b6ba2db682c1
jinxIsOp <154748750+jinxIsOp@users.noreply.github.com>
2024-05-16 21:15:16 +0100

    final commit

--- schedule_manager.py
+++ schedule_manager.py
@@ -7,12 +7,32 @@ class ScheduleManager:
         self.teachers = teachers
 
     def generate_schedules_for_all(self):
-        schedules = {}
+        schedules = []
         for year_name, year_data in self.years.items():
-            year_schedule = {}
+            year_number = int(year_name.split(' ')[0].replace('th', '').replace('nd', '').replace('st', ''))
+            year_schedule = {
+                "year": year_number,
+                "specialite": []
+            }
+            specialities = {}
+            
             sections = year_data.get('sections', [])
             for section_data in sections:
+                speciality = section_data.get('speciality', 'Unknown Speciality')
+                if speciality not in specialities:
+                    specialities[speciality] = {
+                        "name": speciality,
+                        "sections": []
+                    }
+                
                 generator = ScheduleGenerator(year_name, section_data, self.rooms, self.teachers)
-                year_schedule[section_data['name']] = generator.generate_schedule()
-            schedules[year_name] = year_schedule
+                section_schedule = generator.generate_schedule()
+                section_info = {
+                    "name": section_data['name'],
+                    "schedule": section_schedule
+                }
+                specialities[speciality]["sections"].append(section_info)
+            
+            year_schedule["specialite"] = list(specialities.values())
+            schedules.append(year_schedule)
         return schedules
